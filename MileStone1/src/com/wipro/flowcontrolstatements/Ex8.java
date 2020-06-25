030deecdfda27d69bc111494b1dde12fbc5b3063
JOKER <JOKER@VVS-KRISHNA>
2020-06-25 08:22:06 +0530

    flow control statements

--- MileStone1/src/com/wipro/flowcontrolstatements/Ex8.java
+++ MileStone1/src/com/wipro/flowcontrolstatements/Ex8.java
@@ -3,20 +3,18 @@ package com.wipro.flowcontrolstatements;
 public class Ex8 {
 
 	public static void main(String[] args) {
-		try {
-			int n = Integer.parseInt(args[0]);
-			System.out.println("Digit");
-			}
-			catch (Exception NumberFormatException){
 			String str = args[0];
 			char a = str.charAt(0);
 			if ((a >= 65 && a <= 90) || (a >=97 &&  a <= 122 )){
 			System.out.println("Alphabhet");
 			}
+			else if (a >= 48 && a <= 57) {
+				System.out.println("Digit");
+			}
 			else{
 			System.out.println("Special Character");
 			}
-			}
+
 	}
 
 }
