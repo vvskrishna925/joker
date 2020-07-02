ed5421ea422205f2d2d8d89af3bedc9045d59a37
JOKER <JOKER@VVS-KRISHNA>
2020-07-02 16:57:38 +0530

    Interfaces

---
+++ MileStone1/src/live/Test.java
@@ -0,0 +1,20 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Veena v = new Veena();
+		v.play();
+		Saxophone s = new Saxophone();
+		s.play();
+		Playable p = new Veena();
+		p.play();
+		p = new Saxophone();
+		p.play();
+	}
+
+}
