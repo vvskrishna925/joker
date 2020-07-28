b18394fc660bba9a5087a78925c5295f77989ee7
JOKER <JOKER@VVS-KRISHNA>
2020-07-28 10:25:03 +0530

    Junit

--- JunitLearning/src/com/wipro/test/TestStringConcat.java
+++ JunitLearning/src/com/wipro/test/TestStringConcat.java
@@ -15,12 +15,5 @@ public class TestStringConcat {
 		String s2 = "Krishna";
 		assertEquals("VvsKrishna",dt.doStringConcat(s1, s2));
 	}
-	@Test
-	public void test2() {
-		DailyTasks dt = new DailyTasks();
-		String s1 = "Vvs";
-		String s2 = "Krishna";
-		assertEquals("Vvs",dt.doStringConcat(s1, s2));
-	}
-
+	
 }
