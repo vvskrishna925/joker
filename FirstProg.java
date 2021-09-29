0bcc456f3c693bf474beef3132c7164824fc91e8
krishna <vvskrishna1999@gmail.com>
2021-09-29 14:57:26 +0530

    Dca program

---
+++ FirstProg.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+class FirstProg {
+public static void main (String []args) {
+Scanner sc = new Scanner(System.in);
+int x = sc.nextInt();
+int y = sc.nextInt();
+if (y == 2 * x){
+System.out.println("YES");
+}
+else {
+System.out.println("NO");
+}
+sc.close();
+}
+}
+
